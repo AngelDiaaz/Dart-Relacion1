379a15242851b407dd5c82f6e06c3859189212e4
adiaz <a_angel.diaz.aviles@iespablopicasso.es>
2022-09-29 20:33:37 +0200

    Hecho

--- mainapp/MainApp.dart
+++ mainapp/MainApp.dart
@@ -1,3 +1,4 @@
+import '../models/Camion.dart';
 import '../models/Coche.dart';
 import '../models/Motocicleta.dart';
 
@@ -6,11 +7,15 @@ void main(List<String> args) {
 
   Motocicleta m = new Motocicleta("Ducati", "Monster", 2015, "Rojo");
 
+  Camion ca = new Camion("Citroen", "Jumper", 2020, "Blanco");
+
   print('El ' + c.marca + ' ' + c.modelo + ' tiene:');
   print(c.ruedas.toString() + ' ruedas');
 
   print('La ' + m.marca + ' ' + m.modelo + ' tiene:');
   print(m.ruedas.toString() + ' ruedas');
 
+  print(ca.remolque);
+  ca.ponerRemolque();
 
 }
---
+++ models/Camion.dart
@@ -0,0 +1,48 @@
+import 'vehiculos.dart';
+
+class Camion extends Vehiculos{
+    //Propiedades
+    int velocidad = 0;
+    int _km = 0;
+    int ruedas = 4;
+    bool remolque = false;
+
+
+
+    //Constructor
+    Camion(super.marca, super.modelo, super.year, super.color);
+
+    //Getters y setters
+    set modifykm(int km) => this._km = km;
+
+    int get km => this._km;
+    
+
+
+    /**
+    * Método que incrementa la velocidad del coche
+    */
+    @override
+    void acelerar(int incremento){
+      this.velocidad = this.velocidad + incremento;
+      print('La velocidad a incrementado a ' + velocidad.toString() + ' km/h');
+    }
+
+    /**
+     * Método que frena la velocidad del coche
+     */
+    @override
+    void frenar(int frenar){
+      this.velocidad = this.velocidad - frenar;
+      print('La velocidad a bajado a ' + velocidad.toString() + ' km/h');
+    }
+
+    /**
+     * Método que indica que se le ha puesto un remolque al camión
+     */
+    void ponerRemolque(){
+      this.remolque = true;
+      print('Se le ha puesto remolque al camión');
+    }
+
+}
